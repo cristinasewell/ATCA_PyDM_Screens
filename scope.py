a484fa0e6ea21a9e575cfe8fe011c9dc5456d6e5
Cristina Sewell <csewell@slac.stanford.edu>
2020-07-12 04:45:05 -0700

    WIP - added a curve to the waveform with python

--- scope.py
+++ scope.py
@@ -1,24 +1,48 @@
 from pydm import Display
 from os import path
+import json
 
 class Scope(Display):
     def __init__(self, parent=None, args=None, macros=None, ui_filename=None):
         super(Scope, self).__init__(parent=parent, args=args, macros=macros)
         # for different channels
-       # self._curves = {0: {}}
+        device = self._macros.get("DEVICE")
+        if device is not None:
+            ioc = "ca://{}:".format(device)
+        self._curves_bay0 = {0: {"y_channel": ioc+"STR0:STREAM_SLOWSHORT0", "x_channel": None, "name": "MyName", "color": "#55ffff", "lineStyle": 1, "lineWidth": 1, "symbol": None, "symbolSize": 10, "redraw_mode": 2}, 
+                1: {"y_channel": ioc, "x_channel": None, "name": "MyName", "color": "#55ffff", "lineStyle": 1, "lineWidth": 1, "symbol": None, "symbolSize": 10, "redraw_mode": 2},
+                2: {"y_channel": ioc, "x_channel": None, "name": "MyName", "color": "#55ffff", "lineStyle": 1, "lineWidth": 1, "symbol": None, "symbolSize": 10, "redraw_mode": 2}, 
+                3: {"y_channel": ioc, "x_channel": None, "name": "MyName", "color": "#55ffff", "lineStyle": 1, "lineWidth": 1, "symbol": None, "symbolSize": 10, "redraw_mode": 2}}
+
+        self._macros = macros
         self.setup_ui()
+        #self.my_ch = {"y_channel": None, "x_channel": None, "name": "MyName", "color": "#55ffff", "lineStyle": 1, "lineWidth": 1, "symbol": None, "symbolSize": 10, "redraw_mode": 2}
 
     def macros(self):
-        is self._macros is None:
+        if self._macros is None:
             return {}
         return self._macros
 
     def handle_show_curve(self):
         curves = []
+        channel = {}
+
+        device = self._macros.get("DEVICE")
+        if device is not None:
+            ioc = "ca://{}:STR0:STREAM_SLOWSHORT0".format(device)
+
+        #ioc = "ca://{}:STR0:STREAM_SLOWSHORT0".format(device)
+        #self.my_ch["y_channel"] = ioc
+        #self.my_ch["x_channel"] = ""
+        #ch = json.dumps(self.my_ch)
+
+        ch = json.dumps(self._curves_bay0.get(0))
+        print("ioc ", ch)
         if self.ui.channel0_pb.isChecked():
-            curves.append('{"y_channel": "ca://${DEVICE}:STR0:STREAM_SLOWSHORT0", "x_channel":""}')
+            curves.append(ch)
+            print(curves)
         #self.ui.plot.setCurves(curves)
-        self.waveformPlotBay0.plot.setCurves(curves)
+        self.waveformPlotBay0.setCurves(curves)
 
     def setup_ui(self):
         self.ui.channel0_pb.clicked.connect(self.handle_show_curve)
