a9b985c2087dcde0d606394da35ae6d1ffb1604e
Cristina Sewell <csewell@slac.stanford.edu>
2020-07-14 16:07:02 -0700

    WIP - modes of curves are showing fine now

--- scope.py
+++ scope.py
@@ -17,6 +17,12 @@ class Scope(Display):
         # self.define_dotted_curves()
         self.setup_ui()
 
+        try:
+            device = self._macros["DEVICE"]
+        except:
+             logger.error("Please provide a valid macro for the IOC - ex: 'DEVICE=MY_IOC' ")
+             sys.exit(1)
+
     # TODO: Probably you don't need this
     # def macros(self):
     #     if self._macros is None:
@@ -24,55 +30,6 @@ class Scope(Display):
     #     return self._macros
 
     # TODO: We can refactor this to no longer need two dicts
-    # def define_dotted_curves(self):
-    #     try:
-    #         device = self._macros["DEVICE"]
-    #         if device:
-    #             ioc = "ca://{}:".format(device)
-    #
-    #             self._dotted_curves = {
-    #                 0: {"y_channel": ioc+"STR0:STREAM_SLOWSHORT0",
-    #                     "x_channel": None, "name": "CH 0", "color": "#55ffff",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2},
-    #
-    #                 1: {"y_channel": ioc+"STR0:STREAM_SLOWSHORT1",
-    #                     "x_channel": None, "name": "CH 1", "color": "#55ff7f",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2},
-    #
-    #                 2: {"y_channel": ioc+"STR0:STREAM_SLOWSHORT2",
-    #                     "x_channel": None, "name": "CH 2", "color": "#ffff7f",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2},
-    #
-    #                 3: {"y_channel": ioc+"STR0:STREAM_SLOWSHORT3",
-    #                     "x_channel": None, "name": "CH 3", "color": "#ffaa7f",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2},
-    #
-    #                 4: {"y_channel": ioc+"STR1:STREAM_SLOWSHORT0",
-    #                     "x_channel": None, "name": "CH 0", "color": "#55ffff",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2},
-    #
-    #                 5: {"y_channel": ioc+"STR1:STREAM_SLOWSHORT1",
-    #                     "x_channel": None, "name": "CH 1", "color": "#55ff7f",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2},
-    #
-    #                 6: {"y_channel": ioc+"STR1:STREAM_SLOWSHORT2",
-    #                     "x_channel": None, "name": "CH 2", "color": "#ffff7f",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2},
-    #
-    #                 7: {"y_channel": ioc+"STR1:STREAM_SLOWSHORT3",
-    #                     "x_channel": None, "name": "CH 3", "color": "#ffaa7f",
-    #                     "lineStyle": 0, "lineWidth": 1, "symbol": 0,
-    #                     "symbolSize": 4, "redraw_mode": 2}}
-    #     except:
-    #         logger.error("You need to define a DEVICE macro ioc  - ex: -m 'DEVICE=MY_IOC' ")
-    #         sys.exit(1)
 
     def define_curves(self):
         try:
@@ -104,7 +61,7 @@ class Scope(Display):
             logger.error("You need to define a DEVICE macro ioc  - ex: -m 'DEVICE=MY_IOC' ")
             sys.exit(1)
 
-    def handle_show_curve(self):
+    def handle_show_curve_bay0(self):
         # bay 0
         b0_curves = []
         if self._curves:
@@ -117,7 +74,7 @@ class Scope(Display):
 
             if self.ui.bay0Mode0_rb.isChecked():
                 style = {
-                    "lineStyle": 1, "lineWidth": 1, "symbol": 0,
+                    "lineStyle": 0, "lineWidth": 1, "symbol": 0,
                     "symbolSize": 4, "redraw_mode": 2
                 }
 
@@ -132,36 +89,31 @@ class Scope(Display):
                     b0_curves.append(ch)
             self.waveformPlotBay0.setCurves(b0_curves)
 
-            # bay 1
-            b1_curves = []
-            # if curves...
-            if self.ui.b1_channel0_pb.isChecked():
-                if self.bay1_line:
-                    ch = json.dumps(self._curves.get(4))
-                    b1_curves.append(ch)
-                else:
-                    ch = json.dumps(self._dotted_curves.get(4))
-                    b1_curves.append(ch)
-            if self.ui.b1_channel1_pb.isChecked():
-                if self.bay1_line:
-                    ch = json.dumps(self._curves.get(5))
-                    b1_curves.append(ch)
-                else:
-                    ch = json.dumps(self._dotted_curves.get(5))
-                    b1_curves.append(ch)
-            if self.ui.b1_channel2_pb.isChecked():
-                if self.bay1_line:
-                    ch = json.dumps(self._curves.get(6))
-                    b1_curves.append(ch)
-                else:
-                    ch = json.dumps(self._dotted_curves.get(6))
-                    b1_curves.append(ch)
-            if self.ui.b1_channel3_pb.isChecked():
-                if self.bay1_line:
-                    ch = json.dumps(self._curves.get(7))
-                    b1_curves.append(ch)
-                else:
-                    ch = json.dumps(self._dotted_curves.get(7))
+    def handle_show_curve_bay1(self):
+        # bay 1
+        b1_curves = []
+        if self._curves:
+            bay1_curves = self._curves[1]
+
+            style = {
+                "lineStyle": 1, "lineWidth": 1, "symbol": 0,
+                "symbolSize": 4, "redraw_mode": 2
+            }
+
+            if self.ui.bay1Mode0_rb.isChecked():
+                style = {
+                    "lineStyle": 0, "lineWidth": 1, "symbol": 0,
+                    "symbolSize": 4, "redraw_mode": 2
+                }
+
+            b1_pbs = [self.ui.b1_channel0_pb, self.ui.b1_channel1_pb,
+                      self.ui.b1_channel2_pb, self.ui.b1_channel3_pb]
+
+            for idx, button in enumerate(b1_pbs):
+                if button.isChecked():
+                    curve = bay1_curves.get(idx)
+                    curve.update(style)
+                    ch = json.dumps(curve)
                     b1_curves.append(ch)
             self.waveformPlotBay1.setCurves(b1_curves)
     
@@ -179,28 +131,20 @@ class Scope(Display):
 
     def setup_ui(self):
         # bay 0
-        self.ui.b0_channel0_pb.clicked.connect(self.handle_show_curve)
-        self.ui.b0_channel1_pb.clicked.connect(self.handle_show_curve)
-        self.ui.b0_channel2_pb.clicked.connect(self.handle_show_curve)
-        self.ui.b0_channel3_pb.clicked.connect(self.handle_show_curve)
+        self.ui.b0_channel0_pb.clicked.connect(self.handle_show_curve_bay0)
+        self.ui.b0_channel1_pb.clicked.connect(self.handle_show_curve_bay0)
+        self.ui.b0_channel2_pb.clicked.connect(self.handle_show_curve_bay0)
+        self.ui.b0_channel3_pb.clicked.connect(self.handle_show_curve_bay0)
         # bay 1
-        self.ui.b1_channel0_pb.clicked.connect(self.handle_show_curve)
-        self.ui.b1_channel1_pb.clicked.connect(self.handle_show_curve)
-        self.ui.b1_channel2_pb.clicked.connect(self.handle_show_curve)
-        self.ui.b1_channel3_pb.clicked.connect(self.handle_show_curve)
+        self.ui.b1_channel0_pb.clicked.connect(self.handle_show_curve_bay1)
+        self.ui.b1_channel1_pb.clicked.connect(self.handle_show_curve_bay1)
+        self.ui.b1_channel2_pb.clicked.connect(self.handle_show_curve_bay1)
+        self.ui.b1_channel3_pb.clicked.connect(self.handle_show_curve_bay1)
         # modes
-        self.ui.bay0Mode0_rb.clicked.connect(self.handle_show_curve)
-        self.ui.bay0Mode1_rb.clicked.connect(self.handle_show_curve)
-        self.ui.bay1Mode0_rb.clicked.connect(self.handle_show_curve)
-        self.ui.bay1Mode1_rb.clicked.connect(self.handle_show_curve)
+        self.ui.bay0Mode0_rb.clicked.connect(self.handle_show_curve_bay0)
+        self.ui.bay0Mode1_rb.clicked.connect(self.handle_show_curve_bay0)
+        self.ui.bay1Mode0_rb.clicked.connect(self.handle_show_curve_bay1)
+        self.ui.bay1Mode1_rb.clicked.connect(self.handle_show_curve_bay1)
 
     def ui_filename(self):
         return 'scope.ui'
-        # TODO: This check should go at INIT
-        # try:
-        #     device = self._macros["DEVICE"]
-        #     if device:
-        #         return 'scope.ui'
-        # except:
-        #     logger.error("Please provide a valid macro for the IOC - ex: 'DEVICE=MY_IOC' ")
-        #     sys.exit(1)
