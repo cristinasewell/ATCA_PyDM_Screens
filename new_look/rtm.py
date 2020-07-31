e7ebae5b11430c2c05a85b2a0d22a12a09edbe7d
cristinasewell <csewell@slac.stanford.edu>
2020-07-31 12:39:09 -0700

    WIP: trying to setAutoVisible()

--- new_look/rtm.py
+++ new_look/rtm.py
@@ -12,7 +12,7 @@ class Rtm(Display):
     def __init__(self, parent=None, args=None, macros=None, ui_filename=None):
         super(Rtm, self).__init__(parent=parent, args=args, macros=macros)
         self.setup_waveforms()
-        self.set_y_range
+        self.set_y_range()
 
     def setup_waveforms(self):
         self.ui.beam_current_waveform.plotItem.getViewBox().setMouseMode(pg.ViewBox.RectMode)
@@ -20,10 +20,12 @@ class Rtm(Display):
         self.ui.rf_power_waveform.plotItem.getViewBox().setMouseMode(pg.ViewBox.RectMode)
 
     def set_y_range(self):
-        self.ui.beam_current_waveform.plotItem.autoRangeY()
-        self.ui.beam_voltage_waveform.plotItem.updateAutoRange()
-        logger.info(self.ui.beam_current_waveform.pltItem.y_address())
-        self.waveformPlotBay1.plotItem.getViewBox().setMouseMode(pg.ViewBox.RectMode)
+        self.ui.beam_current_waveform.plotItem.getViewBox().setAutoVisible()
+        self.ui.beam_voltage_waveform.plotItem.getViewBox().setAutoVisible()
+        self.ui.rf_power_waveform.plotItem.getViewBox().setAutoVisible()
+        #self.ui.beam_voltage_waveform.plotItem.getViewBox().setAutoRange(padding=0.2)
+        #logger.info(self.ui.beam_current_waveform.pltItem.y_address())
+        #self.waveformPlotBay1.plotItem.getViewBox().setMouseMode(pg.ViewBox.RectMode)
 
 
     def ui_filename(self):
