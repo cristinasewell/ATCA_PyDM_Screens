6f6be953f8d7f19beff60283306f4f0794ca499f
cristinasewell <csewell@slac.stanford.edu>
2020-08-06 21:43:40 -0700

    FIX: added the ZERO option for end and start

--- new_look/define_average_window.py
+++ new_look/define_average_window.py
@@ -4,6 +4,7 @@ import logging
 import pyqtgraph as pg
 from functools import partial
 import scipy.signal.windows
+from numbers import Number
 
 import numpy as np
 from pydm.widgets.pushbutton import PyDMPushButton
@@ -37,7 +38,7 @@ class AverageWindow(Display):
 
     def setup_ui(self):
         self.ui.draw_window_pb.clicked.connect(self.plot_data)
-       # self.ui.get_waveform_pb.clicked.connect(self.handle_show_curves)
+        # self.ui.get_waveform_pb.clicked.connect(self.handle_show_curves)
 
     def define_complex_curves(self):
         try:
@@ -214,8 +215,13 @@ class AverageWindow(Display):
         i_end = self.get_current_i_end()
         i_size = self.get_i_pv_size()
         i_is_valid = False
+        is_i_zeros = False
 
-        if i_start and i_end and i_size:
+        if i_start == 0 and i_end == 0:
+            is_i_zeros = True
+
+        if (isinstance(i_start, Number) and
+                isinstance(i_end, Number) and i_size):
             i_is_valid = True
 
         logger.debug('I start: {}, end: {}'.format(i_start, i_end))
@@ -224,9 +230,14 @@ class AverageWindow(Display):
         q_end = self.get_current_q_end()
         q_size = self.get_q_pv_size()
         q_is_valid = False
+        is_q_zeros = False
 
-        if q_start and q_end and q_size:
+        if (isinstance(q_start, Number) and
+                isinstance(q_end, Number) and q_size):
             q_is_valid = True
+
+        if q_start == 0 and q_end == 0:
+            is_q_zeros = True
 
         logger.debug('Q start: {}, end: {}'.format(q_start, q_end))
 
@@ -236,7 +247,7 @@ class AverageWindow(Display):
         q_pen = pg.mkPen(color=(85, 0, 255))
 
         if i_is_valid:
-            if (i_start >= i_end) or (i_end >= i_size):
+            if (i_start >= i_end and not is_i_zeros) or (i_end >= i_size):
                 self.ui.error_label.setText(
                     "Not a valid I window size. "
                     "Please make sure the start < end, or end < pv_size"
@@ -250,7 +261,7 @@ class AverageWindow(Display):
                 self.ui.average_window_wf.plot(self.i_win, pen=i_pen)
 
         if q_is_valid:
-            if (q_start >= q_end) or (q_end >= q_size):
+            if (q_start >= q_end and not is_q_zeros) or (q_end >= q_size):
                 self.ui.error_label.setText(
                     "Not a valid Q window size."
                     "Please make sure the start < end, or end < pv_size"
@@ -262,6 +273,7 @@ class AverageWindow(Display):
                 self.q_win = [0]*q_start + [1]*(q_end - q_start) + [0]*(q_size - q_end)
                 self._curve_q = self.q_win
                 self.ui.average_window_wf.plot(self.q_win, pen=q_pen)
+
         if not (i_is_valid or q_is_valid):
             self.ui.error_label.setText(
                 "You must define start & end values"
